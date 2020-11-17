50542d085cce454bd99ef668fd09d23a493385f3
Jefta Dirksen <JeftaDirksen@gmail.com>
2020-11-17 19:54:49 +0100

    Rewrite

--- MirrorFiles.ps1
+++ MirrorFiles.ps1
@@ -1,34 +1,36 @@
-$source = "C:\Temp\A"
-$target = "C:\Temp\B"
+$sourcePath = "C:\Test\A"
+$targetPath = "C:\Test\B"
 
-$sourceFiles = Get-ChildItem -Path $source -Recurse
-$targetFiles = Get-ChildItem -Path $target -Recurse
-
-$syncMode = 1
-
-try {
-    $diff = Compare-Object -ReferenceObject $sourceFiles -DifferenceObject $targetFiles
-
-    foreach ($f in $diff) {
-        if ($f.SideIndicator -eq "<=") {
-            $fullSourceObject = $f.InputObject.FullName
-            $fullTargetObject = $f.InputObject.FullName.Replace($source, $target)
-
-            Write-Host "Attemp to copy the following: " $fullSourceObject
-            Copy-Item -Path $fullSourceObject -Destination $fullTargetObject
+# Copy/Create new/modified files/folders
+Get-ChildItem -Path $sourcePath -Recurse | ForEach-Object {
+    $sourceFileName = $_.FullName
+    $targetFileName = $_.FullName.Replace($sourcePath, $targetPath)
+    if ($_.PSIsContainer) {
+        if (-not (Test-Path $targetFileName)) {
+            "New folder {0}" -f $targetFileName
+            New-Item -Path $targetFileName -ItemType "directory" | Out-Null
         }
-
-
-        if ($f.SideIndicator -eq "=>" -and $syncMode -eq 2) {
-            $fullSourceObject = $f.InputObject.FullName
-            $fullTargetObject = $f.InputObject.FullName.Replace($target, $source)
-
-            Write-Host "Attemp to copy the following: " $fullSourceObject
-            Copy-Item -Path $fullSourceObject -Destination $fullTargetObject
+    }
+    else {
+        if (Test-Path $targetFileName) {
+            $targetFile = Get-Item -Path $targetFileName
+            if ($_.LastWriteTime -ne $targetFile.LastWriteTime -or $_.Length -ne $targetFile.Length) {
+                "Modified file {0}" -f $sourceFileName
+                Copy-Item -Path $sourceFileName -Destination $targetFileName
+            }
         }
-
+        else {
+            "New file {0}" -f $sourceFileName
+            Copy-Item -Path $sourceFileName -Destination $targetFileName
+        }
     }
 }
-catch {
-    Write-Error -Message "something bad happened!" -ErrorAction Stop
+
+# Remove extra files/folders
+Get-ChildItem -Path $targetPath -Recurse | ForEach-Object {
+    if (-not (Test-Path $_.FullName.Replace($targetPath, $sourcePath))) {
+        $type = if ($_.PSIsContainer) { "folder" } else { "file" }
+        "Extra {0} {1}" -f $type, $_.FullName
+        Remove-Item -Path $_.FullName -Recurse
+    }
 }
